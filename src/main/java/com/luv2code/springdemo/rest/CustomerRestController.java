092f910f368b099bea05bdbbb6753d43e461a9c5
JTTran <tranjt80@gmail.com>
2021-09-23 20:39:21 +0300

    Add getmapping for get single customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +20,18 @@ public class CustomerRestController {
 	private CustomerService customerService;
 	
 	
-	// add mapping for Get /customers
+	// add mapping for GET /customers
 	@GetMapping("/customers")
 	public List<Customer> getCustomers() {		
 		return customerService.getCustomers();
+	}
+	
+	// add mapping for GET /customers/{customerId}
+	@GetMapping("/customers/{customerId}")
+	public Customer getCustomer(@PathVariable int customerId) {
+		Customer theCustomer = customerService.getCustomer(customerId);
+		
+		return theCustomer;
 	}
 }
 
