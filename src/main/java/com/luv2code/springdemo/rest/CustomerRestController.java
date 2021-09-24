059fffdc3173d1a55e436687facdec13206de819
JTTran <tranjt80@gmail.com>
2021-09-24 20:58:39 +0300

    Added Exception handling for customer not found

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -14,27 +14,27 @@ import com.luv2code.springdemo.service.CustomerService;
 @RestController
 @RequestMapping("/api")
 public class CustomerRestController {
-	
+
 	// autowire the CustomerService
 	@Autowired
 	private CustomerService customerService;
-	
-	
+
 	// add mapping for GET /customers
 	@GetMapping("/customers")
-	public List<Customer> getCustomers() {		
+	public List<Customer> getCustomers() {
 		return customerService.getCustomers();
 	}
-	
+
 	// add mapping for GET /customers/{customerId}
 	@GetMapping("/customers/{customerId}")
 	public Customer getCustomer(@PathVariable int customerId) {
 		Customer theCustomer = customerService.getCustomer(customerId);
+
+		if (theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
 		
 		return theCustomer;
 	}
+
 }
-
-
-
-
